fec26152cb8972f43471957c5b89a997f5b69a61
exe-apps <edwincruz777@gmail.com>
2020-05-10 16:10:04 +0800

    added sun endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,9 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/sun', function (req, res) {
+  res.send('Hello Sun!\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
